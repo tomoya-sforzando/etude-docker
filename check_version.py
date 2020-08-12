c59f990addd4acd37055d2c0837387ce4c18faa6
Tomoya KASHIMADA <tomoya@sforzando.co.jp>
2020-08-12 12:57:42 +0900

    Change sample code

--- check_version.py
+++ check_version.py
@@ -1,5 +1,9 @@
-
 #!/usr/bin/env python3
 import sys
-print(__name__)
 print(sys.version)
+
+old, new = 0, 1
+for i in range(10):
+    print(f"{i=}, {old=}, {new=}")
+    old = new
+    new = old + new
--- init.py
+++ init.py
@@ -1,2 +1,4 @@
 
 #!/usr/bin/env python3
+import os
+os.makedirs("output", exist_ok=True)
